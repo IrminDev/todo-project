d5e31cd1691996d82e3d5434222a50f6e026c477
IrminDev <ihernandezj1902@alumno.ipn.mx>
2024-08-05 21:06:35 -0600

    Testing tagged commits

--- todo-app/src/main/java/com/github/irmindev/todo_app/TodoAppApplication.java
+++ todo-app/src/main/java/com/github/irmindev/todo_app/TodoAppApplication.java
@@ -3,8 +3,6 @@ package com.github.irmindev.todo_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// This is for test commit
-
 @SpringBootApplication(scanBasePackages = "com.github.irmindev.todo_app")
 public class TodoAppApplication {
 	public static void main(String[] args) {
